ebb97427cb62f3f94934da03d5371c75e9b6b161
Brian M Hunt <brianmhunt@gmail.com>
2017-03-23 17:01:59 -0400

    gcloud + yarn + diff-so-fancy

--- setup-osx/gcloud.sh
+++ setup-osx/gcloud.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-set fish_user_paths /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin
+set -U fish_user_paths /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin
 
 set -x MANPATH /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/help/man /usr/local/share/man /usr/share/man /opt/x11/share/man
 
 fish_update_completions
 
+
+gcloud components install cloud-datastore-emulator app-engine-python cbt
 
 
 
