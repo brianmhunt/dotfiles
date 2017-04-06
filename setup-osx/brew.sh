aa3f6415ada75025e5277497cc3b24c7a37a833f
Brian M Hunt <bmh@tko.io>
2017-04-06 18:12:29 -0400

    Add icdiff to gitconfig

--- setup-osx/brew.sh
+++ setup-osx/brew.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-brew install fish chromedriver curl wget git watch node caskroom/cask/brew-cask vim gpg2 yarn diff-so-fancy
+brew install fish chromedriver curl wget git watch node caskroom/cask/brew-cask vim gpg2 yarn diff-so-fancy icdiff
--- setup-osx/git.sh
+++ setup-osx/git.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# TODO Copy/symlink gitconfig => ~/.gitconfig
 git config --global user.name "Brian M Hunt"
 git config --global user.email brianmhunt@gmail.com
 git config --global pager.diff "diff-so-fancy | less --tabs=1,5 -RFX"
@@ -8,5 +9,16 @@ git config --global commit.gpgsign true
 git config --global user.signingkey 3F33B0B6
 git config --global gpg.program /usr/local/bin/gpg
 
+git config --global diff.tool icdiff
+git config --global difftool.prompt false
+# [difftool "icdiff"]
+#  cmd = /usr/local/bin/icdiff --line-numbers $LOCAL $REMOTE
+
+
+
+# glog = log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'
+# conflictstyle = diff3 
+# showUntrackedFiles = all
+# fsckobjects = true
 # set -U HOMEBREW_GITHUB_API_TOKEN ***
 
