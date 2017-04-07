c616f0ecae5c0ae1da3b35bbf24c08a21a7ed8fc
Brian M Hunt <brianmhunt@gmail.com>
2017-04-07 11:12:30 -0400

    Add monity helper

--- setup-osx/cask.sh
+++ setup-osx/cask.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-brew cask install tower telegram dropbox spotify iterm2 slack atom mactex firefox mactracker sublime-text transmit dash vmware-fusion google-cloud-sdk sync gpg-tools
+brew cask install tower telegram dropbox spotify iterm2 slack atom mactex firefox mactracker sublime-text transmit dash vmware-fusion google-cloud-sdk sync gpg-tools monity-helper
 
 brew tap caskroom/versions
 brew cask install google-chrome-beta
