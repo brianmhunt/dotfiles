df988c719f9bcc724ad0f73c05b7ddbd2eb19925
Brian M Hunt <bmh@tko.io>
2017-10-16 16:27:16 -0400

    Add save_packages for saving brews & atoms

--- setup-osx/brew.sh
+++ setup-osx/brew.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-brew install fish chromedriver curl wget git watch node caskroom/cask/brew-cask vim gpg2 yarn diff-so-fancy icdiff httpie pinentry-mac
+BREWS=`cat brews.txt`
+
+brew install $BREWS 
---
+++ setup-osx/save_packages.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ls ~/.atom/packages > atoms.txt
+brew list > brews.txt
+
