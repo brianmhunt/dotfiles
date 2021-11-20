ee8e3e9e3f5c49ec91ead087313d259e1fe61548
Brian M Hunt <brian@minutebox.com>
2021-11-20 09:43:06 -0500

    update 2021/setup.sh

--- 2021/setup.sh
+++ 2021/setup.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
-FONTS := font-cascadia-code-pl font-firacode-nerd-font font-cascadia-mono-pl font-hasklig font-jetbrainsmono-nerd-font
-UTILS := ripgrep git starship little-snitch gh
-COMPILERS := go node@16
-APPS := 1password zoom slack 
-BROWSERS := google-chrome-beta firefox-developer-edition safari-technology-preview
+FONTS="font-cascadia-code-pl font-fira-code-nerd-font font-cascadia-mono-pl font-hasklig font-jetbrains-mono-nerd-font"
+UTILS="ripgrep git starship little-snitch gh google-cloud-sdk wget httpie"
+COMPILERS="go node@16"
+APPS="1password zoom slack iterm2 alfred visual-studio-code"
+BROWSERS="google-chrome-beta firefox-developer-edition safari-technology-preview"
 
+# Others:
+# transmit
+# gpg-suite
+# dropbox (awaiting M1)
+# docker
+# dash
+# brave-browser
+# zsh
+# zsh-autosuggestions
+# zsh-completions
+# zsh-git-prompt
+# zsh-syntax-highlighting
+# pinentry-mac
+# nmap 
+
+brew tap homebrew/cask-versions
+brew tap homebrew/cask-fonts
 brew install $FONTS $UTILS $COMPILERS $APPS $BROWSERS
 
